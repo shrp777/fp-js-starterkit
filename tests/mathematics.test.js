e791f2b9290276fbda7f4dbd2c8b3e608853905c
Alexandre Leroux <alex@sherpa.one>
2023-09-25 18:01:10 +0200

    improve tests

--- tests/mathematics.test.js
+++ tests/mathematics.test.js
@@ -1,13 +1,22 @@
 import { describe, expect, test } from "@jest/globals";
 import { add, divide } from "../src/mathematics";
 
-describe("Some dummy tests suite", () => {
+describe("mathematics add", () => {
   test("add(1,1) should return 2", () => {
     expect(add(1, 1)).toEqual(2);
   });
   test("add('a','b') should return Error", () => {
     expect(add("a", "b")).toBeInstanceOf(Error);
   });
+  test("add('a','b') should return Error", () => {
+    expect(add("a", null)).toBeInstanceOf(Error);
+  });
+  test("add('a','b') should return Error", () => {
+    expect(add("a", undefined)).toBeInstanceOf(Error);
+  });
+});
+
+describe("mathematics divide", () => {
   test("divide(10)(2) should return 5", () => {
     expect(divide(10)(2)).toEqual(5);
   });
